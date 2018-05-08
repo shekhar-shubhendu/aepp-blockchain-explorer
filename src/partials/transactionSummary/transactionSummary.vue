ad7e6af2aca92dccd4880820a5962e625b5e6b45
SamLife91 <sbarguti@quanterall.com>
2018-05-08 13:33:58 +0300

    init transaction summary component

---
+++ src/partials/transactionSummary/transactionSummary.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tx-summary">
+    <div class="tx-summary-title">
+      <h1>
+        transaction summary
+      </h1>
+    </div>
+    <div class="tx-summary-data">
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+      <div class="tx-summary-data-row">
+        <div class="tx-summary-data-row-item">1</div>
+        <div class="tx-summary-data-row-item">2</div>
+      </div>
+    </div>
+  </div>
+  <div class="tx-overlay"></div>
+</template>
+<script>
+  export default {
+    name: 'transactionSummary',
+    props: [
+      'tx'
+    ]
+  }
+</script>
+<style lang='scss'>
+  .tx-summary-data-row {
+    display: flex;
+  }
+</style>
